b74b2d3ac9cadb846d422ede3da41ff85e99f294
ih4t3youall <ih4t3youall@gmail.com>
2019-10-08 17:21:33 -0300

    algunas entrevistas

---
+++ proyectos/rachio/agustinChirichigno/coreRequirements.cr
@@ -0,0 +1,38 @@
+List of requirements:
+•	Minimum 5-7 years of professional industry experience
+•	Strong Object-Oriented development background or knowledge
+•	AngularJS
+•	HTML5, CSS3
+•	JUNIT framework
+•	5-7 years of SQL
+•	Web services development
+•	Familiarity with JSON and XML
+•	Exposure to Java and back end development
+•	Spring Framework
+•	Maven
+•	Ubuntu
+•	AWS
+•	Strong interpersonal skills
+•	Exposure and understanding of webservers like Tomcat or similar
+•	Ability to perform in a fast paced team environment using agile methodologies
+•	Takes ownership and pride in delivering a product that works as promised
+•	Must be able to work in a dynamic fast-paced environment
+•	Bachelor's Degree in Computer Science, Computer Information Systems, Information Technology, or equivalent
+2.	This is a full-time and remote position to work from Argentina.
+Core requirements
+1.	Java developer with in-depth experience in Object-Oriented Analysis and Design, Design Patterns, Application Frameworks.
+2.	Knowledge of restful web services.
+3.	Strong experience working with Tomcat, Spring Framework and Hibernate.
+4.	Strong experience using Servlets/JSP tags.
+5.	Able to independently architect solutions and recommend design options from requirements.
+6.	Strong knowledge of database and ability to write SQL in Oracle and MySQL and optimize queries.
+7.	Experience developing Junit tests.
+ 
+ Nice to have skills
+ 1.	Strong focus on quality, code reviews, architecture and design.
+ 2.	Experience with AWS Lambda/NodeJs.
+ 	- s3, se2,lambda function.
+ 3.	Background in finance/trading, good communication skills and usage of tools such as GIT and JIRA.
+ 4.	Experience interfacing with Content Management Systems (CQ5, OpenCMS, AEM
+ 		-sql,mysql,postgre,sqllite.
+		-oracle no.
